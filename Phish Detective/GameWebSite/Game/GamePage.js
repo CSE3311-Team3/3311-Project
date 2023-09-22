d4f7c559b24c8be6267d45b3071731afe7a8f1ff
Khorva <102830271+Khorva@users.noreply.github.com>
2023-09-22 18:39:03 -0500

    Added EndScreen page. Still need to apply styling

--- Phish Detective/GameWebSite/Game/GamePage.js
+++ Phish Detective/GameWebSite/Game/GamePage.js
@@ -1,6 +1,26 @@
-﻿window.addEventListener('load', () => {
+﻿var QuestionNumber = 1;
 
+window.addEventListener('load', () => {
     const playerName = localStorage.getItem('playerName');
     document.getElementById("name").innerHTML = playerName;
-
 })
+
+
+function phishClick() {
+    QuestionNumber += 1;
+    document.getElementById("ChoiceSelected").innerHTML = "Phish Button was selected()";
+    updateQuestionCount();
+}
+
+function realClick() {
+    QuestionNumber += 1;
+    document.getElementById("ChoiceSelected").innerHTML = "Real Button was selected()";
+    updateQuestionCount();
+}
+
+function updateQuestionCount() {
+    if (QuestionNumber == 11) {
+        window.location.href = "/EndScreen/EndScreen.html";
+    }
+    document.getElementById("QuestionNumber").innerHTML = QuestionNumber;
+}
