0b61d85f9e225a698da1eb7713b913745d17f572
Khorva <102830271+Khorva@users.noreply.github.com>
2023-09-23 13:15:48 -0500

    Figured out how to import object classes. Still need to do styling

--- Phish Detective/GameWebSite/Game/GamePage.js
+++ Phish Detective/GameWebSite/Game/GamePage.js
@@ -1,26 +1,34 @@
-﻿var QuestionNumber = 1;
+﻿import { Email } from "./Email.js";
+
+var QuestionNumber = 1;
+var testEmail;
 
 window.addEventListener('load', () => {
     const playerName = localStorage.getItem('playerName');
     document.getElementById("name").innerHTML = playerName;
+    document.getElementById("QuestionNumber").innerHTML = QuestionNumber;
+    document.getElementById("Phish_button").addEventListener("click", phishClick);
+    document.getElementById("Real_button").addEventListener("click", realClick);
+    testEmail = new Email(playerName, "Any Sender");
+    console.log(testEmail);
 })
 
-
 function phishClick() {
-    QuestionNumber += 1;
-    document.getElementById("ChoiceSelected").innerHTML = "Phish Button was selected()";
     updateQuestionCount();
+    document.getElementById("ChoiceSelected").innerHTML = "Phish Button was selected()";
 }
 
 function realClick() {
-    QuestionNumber += 1;
-    document.getElementById("ChoiceSelected").innerHTML = "Real Button was selected()";
     updateQuestionCount();
+    document.getElementById("ChoiceSelected").innerHTML = "Real Button was selected()";
 }
 
 function updateQuestionCount() {
-    if (QuestionNumber == 11) {
-        window.location.href = "/EndScreen/EndScreen.html";
+    if (QuestionNumber == 10) {
+        window.location.href = "../EndScreen/EndScreen.html";
     }
-    document.getElementById("QuestionNumber").innerHTML = QuestionNumber;
+    else {
+        QuestionNumber += 1;
+        document.getElementById("QuestionNumber").innerHTML = QuestionNumber;
+    }
 }
